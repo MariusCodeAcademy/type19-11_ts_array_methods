62929bcbae8bb3828043a6e8d756f76685bd00bd
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-23 10:26:35 +0200

    uzd

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
 console.log('Hello from app.ts!');
+
+// importuoti students
+
+// iskonsolinti pirma studenta
+
+// sukurti funkcija kuri atspausdina ir grazina prima studenta
+// argumnetu primima studentu masyva
