7b730ac861351b9ea93308002580260527665e68
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-23 10:32:55 +0200

    uzd

--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,12 @@ console.log('Hello from app.ts!');
 
 // sukurti funkcija kuri atspausdina ir grazina prima studenta
 // argumnetu primima studentu masyva
+
+// sukurti funkcija kuri ima studentu masyva kaip argumenta ir:
+
+// 1. grazina nauja masyva studentus kurie turi masina
+// 2. grazina nauja masyva su zmonemis is Vilniaus
+// 3. grazina nauja masyva kuriame yra moterys
+// 4. grazina true jei yra nors vienas zmogus is Kauno
+// 4.1 ima antra argumenta miesta grazina true jei yra nors vienas zmogus is argumentu gauto miesto. isTherePersonFrom('Vilnius')
+// 4.2 atlikti [4.1] bet kad veiktu nepriklausomai nuo didziuju ir mazuju raidziu
